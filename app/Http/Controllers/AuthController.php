2ed48ff9df6ff1cac0ffbd7c22ef79eb632fc519
damlanureldemir <damlanureldemir@gmail.com>
2023-05-23 21:41:17 +0300

    AuthController oluşturuldu

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(){
+        return view('admin.auth.admin_login');
+    }
+    public function loginPost(Request $request){
+        if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+            return view('panel'); die;
+        }
+        return redirect()->route('admin.login')->withErrors('Email veya şifre hatalı');
+    }
+
+    public function logout(){
+
+        Auth::logout();
+        return redirect()->route('admin.login');
+    }
+}

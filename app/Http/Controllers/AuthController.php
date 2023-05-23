cbda656df69691b0fa2cd206e49845f10dc60e23
beyzanur basatogrul <beyza.basatogrul12@gmail.com>
2023-05-24 02:36:34 +0300

    giris sayfasi olusturuldu auth islemleri yapıldı

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,17 +8,21 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         return view('admin.auth.admin_login');
     }
-    public function loginPost(Request $request){
-        if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
-            return view('panel'); die;
+
+    public function loginPost(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return view('panel');
         }
         return redirect()->route('admin.login')->withErrors('Email veya şifre hatalı');
     }
 
-    public function logout(){
+    public function logout()
+    {
 
         Auth::logout();
         return redirect()->route('admin.login');

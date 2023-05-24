e211aa0b8a7dac70eb70845790e3e70d90020a69
damlanureldemir <damlanureldemir@gmail.com>
2023-05-24 21:41:53 +0300

    user sayfasında hata düzeltildi

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,200 +1,173 @@
-
 @extends('front.master')
 @section('content')
 
-    <div class="col-lg-12 grid-margin stretch-card" xmlns="http://www.w3.org/1999/html">
+    <div class="col-lg-12 grid-margin stretch-card">
         <div class="card">
             <div class="card-body">
                 <li class="col-2 width-right nav-item dropdown d-none d-lg-block">
-
+                    <a href="" type="button" class="nav-link btn btn-success create-new-button">+ Yeni Kasiyer Ekle</a>
                 </li><br><br>
                 <h4 class="card-title">Kasiyerler</h4>
                 <div class="table-responsive">
                     <table class="table table-striped">
-
                         <thead>
                         <tr>
-                            <th>#</th>
-                            <th>İsim</th>
-                            <th>Email</th>
-                            <th>Telefon Numarası</th>
-                            <th>Rol</th>
-                            <th>İşlem</th>
+                            <th>
+                                User
+                            </th>
+                            <th>
+                                First name
+                            </th>
+                            <th>
+                                Progress
+                            </th>
+                            <th>
+                                Amount
+                            </th>
+                            <th>
+                                Deadline
+                            </th>
                         </tr>
                         </thead>
-                    </table>
-
-                    <tbody>
-                    @foreach($users as $key=>$user)
+                        <tbody>
                         <tr>
-                            <td>
-                                {{$key+1}}
-                            </td>
-
-                            <td>
-                                {{$user->name}}
+                            <td class="py-1">
+                                <img src="../../images/faces/face1.jpg" alt="image"/>
                             </td>
                             <td>
-                                {{$user->email}}
+                                Herman Beck
                             </td>
                             <td>
-                                @if      ($user->is_admin==1)         Admin
-                                @else
-                                    Kasiyer
-                                @endif
+                                <div class="progress">
+                                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
                             </td>
-
-
+                            <td>
+                                $ 77.99
+                            </td>
+                            <td>
+                                May 15, 2015
+                            </td>
+                        </tr>
                         <tr>
-                            <div class ="btn-group">
-                                <a href="#"class="btn btn-info btnt-sm " data-toggle="modal"
-                                   data-target="#editUser"{{$user->id}}><i class="fa fa-edit">
-                                    </i>Düzenle</a>
-                                <a href="#"  data-toggle="modal" data-target=#deleteUser  class="btn btn-sm btn-danger">Sil</a>
-                            </div>
+                            <td class="py-1">
+                                <img src="../../images/faces/face2.jpg" alt="image"/>
+                            </td>
+                            <td>
+                                Messsy Adam
+                            </td>
+                            <td>
+                                <div class="progress">
+                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </td>
+                            <td>
+                                $245.30
+                            </td>
+                            <td>
+                                July 1, 2015
+                            </td>
                         </tr>
+                        <tr>
+                            <td class="py-1">
+                                <img src="../../images/faces/face3.jpg" alt="image"/>
+                            </td>
+                            <td>
+                                John Richards
+                            </td>
+                            <td>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </td>
+                            <td>
+                                $138.00
+                            </td>
+                            <td>
+                                Apr 12, 2015
+                            </td>
                         </tr>
-
-                        <div class="modal right fade" id="editUser"{{$user->id}} data-backdrop="static"
-                             aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h4 class="modal-title" id="staticBackdropLabel">Kasiyeri Düzenle</h4>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label>
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                        {{$user->id}}
-                                    </div>
+                        <tr>
+                            <td class="py-1">
+                                <img src="../../images/faces/face4.jpg" alt="image"/>
+                            </td>
+                            <td>
+                                Peter Meggik
+                            </td>
+                            <td>
+                                <div class="progress">
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="modal-body">
-                            <form action="{{route('users.update',$user->id)}}" method="POST">
-                                @csrf
-                                @method('put')
-                                <div class="form-group">
-                                    <label for="">İsim</label>
-                                    <input type="text" name="name" id="" value="{{$user->name}}" class="form-control">
+                            </td>
+                            <td>
+                                $ 77.99
+                            </td>
+                            <td>
+                                May 15, 2015
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="py-1">
+                                <img src="../../images/faces/face5.jpg" alt="image"/>
+                            </td>
+                            <td>
+                                Edward
+                            </td>
+                            <td>
+                                <div class="progress">
+                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="">Email</label>
-                                    <input type="email" name="email" id="" value="{{$user->email}}" class="form-control">
+                            </td>
+                            <td>
+                                $ 160.25
+                            </td>
+                            <td>
+                                May 03, 2015
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="py-1">
+                                <img src="../../images/faces/face6.jpg" alt="image"/>
+                            </td>
+                            <td>
+                                John Doe
+                            </td>
+                            <td>
+                                <div class="progress">
+                                    <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
-                                {{--<div class="form-group">
-                                    <label for="">Telefon Numarası</label>
-                                    <input type="text" name="phone" id=""value="{{$user->phone}}"  class="form-control">
-                                </div>--}}
-                                <div class="form-group">
-                                    <label for="">Şifre</label>
-                                    <input type="password" name="password" id=""readonly value="{{$user->password}}" class="form-control">
+                            </td>
+                            <td>
+                                $ 123.21
+                            </td>
+                            <td>
+                                April 05, 2015
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="py-1">
+                                <img src="../../images/faces/face7.jpg" alt="image"/>
+                            </td>
+                            <td>
+                                Henry Tom
+                            </td>
+                            <td>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
-                                {{--<div class="form-group">
-                                    <label for="">Şifreyi Onayla</label>
-                                    <input type="password" name="password" id="" class="form-control">
-                                </div>--}}
-                                <div class="form-group">
-                                    <label for="">Role</label>
-                                    <select name="is_admin" id="" class="form-control">
-                                        <option value="1" @if($user->is_admin==1)
-                                            selected
-                                            @endif>Admin</option>
-                                        <option value="2" @if($user->is_admin==2)
-                                            selected
-                                            @endif>Kasiyer</option>
-                                    </select>
-                                </div>
-                                <div class="modal-footer">
-                                    <button class="btn btn-warning btn-block" type="submit">Güncelle</button>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal right fade" id="deleteUser"{{$user->id}} data-backdrop="static"
-                             aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h4 class="modal-title" id="staticBackdropLabel">Kasiyeri Sil</h4>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label>
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                        {{$user->id}}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-body">
-                            <form action="{{route('users.delete',$user->id)}}" method="POST">
-                                @csrf
-                                @method('delete')
-                                <p>   Silmek istediğinize emin misiniz {{$user->name}}?</p>
-                                <div class="modal-footer">
-                                    <button class="btn btn-default" data-dismiss="modal">Kapat</button>
-                                    <button type="submit" class="btn btn-danger">Sil</button>
-                                </div>
-                            </form>
-                        </div>
-
-
-                    </tbody>
-                    @endforeach
+                            </td>
+                            <td>
+                                $ 150.00
+                            </td>
+                            <td>
+                                June 16, 2015
+                            </td>
+                        </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
         </div>
-        <div class="modal right fade" id="addUser"{{$user->id}} data-backdrop="static"
-             aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title" id="staticBackdropLabel">Kasiyeri Düzenle</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        {{$user->id}}
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="modal-body">
-            <form action="{{route('users.store',$user->id)}}" method="POST">
-                @csrf
-                @method('put')
-                <div class="form-group">
-                    <label for="">İsim</label>
-                    <input type="text" name="name" id="" value="{{$user->name}}" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Email</label>
-                    <input type="email" name="email" id="" value="{{$user->email}}" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Telefon Numarası</label>
-                    <input type="text" name="phone" id=""value="{{$user->phone}}"  class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Şifre</label>
-                    <input type="password" name="password" id=""value="{{$user->password}}" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Şifreyi Onayla</label>
-                    <input type="password" name="password" id="" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Role</label>
-                    <select name="is_admin" id="" class="form-control">
-                        <option value="1" @if($user->is_admin==1)
-                            selected
-                            @endif>Admin</option>
-                        <option value="2" @if($user->is_admin==2)
-                            selected
-                            @endif>Kasiyer</option>
-                    </select>
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-warning btn-block" type="submit">Güncelle</button>
+    </div>
 
-                </div>
-            </form>
-        </div>
 @endsection
